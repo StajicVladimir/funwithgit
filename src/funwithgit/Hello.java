9921ef103fd6bb8d34c8f249387555982e691aee
Vladimir <vstajic@gmail.com>
2017-02-27 21:47:25 +0100

    Added Heroes to add-heroes branch
    This should be first commit on feature branch

--- src/funwithgit/Hello.java
+++ src/funwithgit/Hello.java
@@ -3,7 +3,11 @@ package funwithgit;
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git! What's up?");
+		
 		HairyMonster momo = new HairyMonster("momo");
 		momo.sayHi();
+		
+		Hero beardyBeard = new Hero("Beardy Beard");
+		beardyBeard.sayHi();
 	}
 }
---
+++ src/funwithgit/Hero.java
@@ -0,0 +1,12 @@
+package funwithgit;
+
+public class Hero {
+	private String name;
+	
+	public Hero(String name){
+		this.name=name;
+	}
+	public void sayHi(){
+		System.out.println("More Beer!");
+	}
+}
