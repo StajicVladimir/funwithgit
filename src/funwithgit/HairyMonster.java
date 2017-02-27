e7f18fcc51944f8f48fa681311bb709031c7478a
Vladimir <vstajic@gmail.com>
2017-02-27 21:53:59 +0100

    Hairy monster smash!
    commit no 2 on feature branch

--- src/funwithgit/HairyMonster.java
+++ src/funwithgit/HairyMonster.java
@@ -6,8 +6,19 @@ public class HairyMonster {
 	public HairyMonster(String name){
 		this.name=name;
 	}
+	
 	public void sayHi(){
 		System.out.println("ROOOOAAAARRR!!");
+	}
+	
+	public void sayName(){
+		System.out.println("Bwwraaarrr "+name+" smaaasshhh!");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
--- src/funwithgit/Hello.java
+++ src/funwithgit/Hello.java
@@ -4,8 +4,9 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git! What's up?");
 		
-		HairyMonster momo = new HairyMonster("momo");
+		HairyMonster momo = new HairyMonster("Momo");
 		momo.sayHi();
+		momo.sayName();
 		
 		Hero beardyBeard = new Hero("Beardy Beard");
 		beardyBeard.sayHi();
