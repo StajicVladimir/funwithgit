0c4a035e18421abcd4ba16721cbe10685f53a7f4
Vladimir <vstajic@gmail.com>
2017-02-27 21:39:03 +0100

    added Hairy Monsters!!
    This is commit number two in master

---
+++ src/funwithgit/HairyMonster.java
@@ -0,0 +1,13 @@
+package funwithgit;
+
+public class HairyMonster {
+	private String name;
+	
+	public HairyMonster(String name){
+		this.name=name;
+	}
+	public void sayHi(){
+		System.out.println("ROOOOAAAARRR!!");
+	}
+	
+}
--- src/funwithgit/Hello.java
+++ src/funwithgit/Hello.java
@@ -3,5 +3,7 @@ package funwithgit;
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git! What's up?");
+		HairyMonster momo = new HairyMonster("momo");
+		momo.sayHi();
 	}
 }
